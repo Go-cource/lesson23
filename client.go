2466f996b927f5a54b5f9590c050a6f4220b14df
Fisherman August <fshrmn.august@gmail.com>
2025-12-25 21:32:53 +0300

    server

---
+++ client.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+
+}
